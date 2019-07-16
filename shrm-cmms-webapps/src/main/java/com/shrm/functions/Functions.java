84e01fbad034753bb30572ad2a6fa434f0b4a168
blairscott <1991038197@qq.com>
2019-07-17 00:42:02 +0800

    ssm pom配置以及controller类

--- shrm-cmms-webapps/src/main/java/com/shrm/functions/Functions.java
+++
@@ -1,7 +0,0 @@
-package com.shrm.functions;
-
-public class Functions {
-    public static void main(String[] args) {
-        System.out.println("...");
-    }
-}
--- shrm-cmms-webapps/src/test/java/com/shrm/test/TestFunctions.java
+++
@@ -1,5 +0,0 @@
-package com.shrm.test;
-
-public class TestFunctions {
-
-}
