9f03ae98529421ab04fbc2f83b73905427359189
blairscott <1991038197@qq.com>
2019-08-06 23:36:16 +0800

    jedis -- set

--- shrm-cmms-webapps/src/test/java/com/shrm/test/JedisSetTest.java
+++ shrm-cmms-webapps/src/test/java/com/shrm/test/JedisSetTest.java
@@ -80,4 +80,70 @@ public class JedisSetTest {
         jedis.del("myset1");
         jedis.del("myset2");
     }
+
+    /*集合运算并将结果存储*/
+    @Test
+    public void setOperationAndStore() {
+        jedis.del("myset1");
+        jedis.del("myset2");
+
+        jedis.sadd("myset1", "a", "b", "c", "d"); // a b c d
+        jedis.sadd("myset2", "c", "d", "e", "f"); // c d e f
+
+        // sdiffstore destination key1 key2 ...
+        Long sdiffstore = jedis.sdiffstore("myset3", "myset1", "myset2");
+        // sinterstore destination key1 key2 ...
+        Long sinterstore = jedis.sinterstore("myset4", "myset1", "myset2");
+        // sunionstore destination key1 key2 ...
+        Long sunionstore = jedis.sunionstore("myset5", "myset1", "myset2");
+
+
+        jedis.del("myset1");
+        jedis.del("myset2");
+        jedis.del("myset3");
+        jedis.del("myset4");
+        jedis.del("myset5");
+    }
+
+    /*查看set的长度*/
+    @Test
+    public void scard() {
+        jedis.del("myset");
+        jedis.sadd("myset","a","b","c");
+
+        // scard key
+        Long myset = jedis.scard("myset"); // 3
+
+        jedis.del("myset");
+    }
+
+    /*随机获得set中的一个成员*/
+    @Test
+    public void randomMember() {
+        jedis.del("myset");
+        jedis.sadd("myset","a","b","c");
+
+        // srandmember key
+        String myset = jedis.srandmember("myset");
+
+        // 没有从set中抽离出去
+        Set<String> myset1 = jedis.smembers("myset"); // a b c
+    }
+
+    @Test
+    public void test() {
+        jedis.del("test1");
+        jedis.del("test2");
+
+        jedis.sadd("test1","a","b","c");
+        jedis.sadd("test2","1","2","3");
+
+        jedis.del("test?");
+
+        System.out.println(jedis.scard("test1"));
+        System.out.println(jedis.scard("test2"));
+
+        jedis.del("test1");
+        jedis.del("test2");
+    }
 }
