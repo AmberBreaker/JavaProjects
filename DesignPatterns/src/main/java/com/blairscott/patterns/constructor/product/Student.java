6591af8a4e622cec1112285ffa69bd7e684f06f2
blairscott <1991038197@qq.com>
2020-03-31 23:42:25 +0800

    构建者模式

---
+++ DesignPatterns/src/main/java/com/blairscott/patterns/constructor/product/Student.java
@@ -0,0 +1,65 @@
+package com.blairscott.patterns.constructor.product;
+
+public class Student {
+
+    private String name;
+
+    private int age;
+
+    private char sex;
+
+    private int score;
+
+    private Father father;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public Father getFather() {
+        return father;
+    }
+
+    public void setFather(Father father) {
+        this.father = father;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", sex=" + sex +
+                ", score=" + score +
+                ", father=" + father +
+                '}';
+    }
+}
